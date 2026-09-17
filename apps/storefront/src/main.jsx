a3eb00a24f9b1cf86b6b5318ac101ce5741f1b3b
parham <p.membari96@gmail.com>
2026-09-17 08:11:05 +0200

    Load storefront font system

--- apps/storefront/src/main.jsx
+++ apps/storefront/src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
+import "./styles/fonts.css";
 import "./styles/search.css";
 import "./styles/shop.css";
 import "./styles/home.css";
